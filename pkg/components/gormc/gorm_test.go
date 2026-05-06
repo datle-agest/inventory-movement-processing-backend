a8ebb1ecadb8556188aafef0fdc16d8e4ecfa4bd
BaoLe1703 <IntAGT26.02.012@agest.vn>
2026-05-06 10:05:34 +0700

    test: add test for ginc, gormc and jwtc

---
+++ pkg/components/gormc/gorm_test.go
@@ -0,0 +1,63 @@
+package gormc
+
+import (
+	sctx "inventory-movement-processing/pkg/service_context"
+	"os"
+	"testing"
+)
+
+var testServiceCtx sctx.ServiceContext
+
+func TestMain(m *testing.M) {
+	_ = os.Setenv("DB_DRIVER", "sqlite")
+	_ = os.Setenv("DB_DSN", ":memory:")
+
+	testServiceCtx = sctx.NewServiceContext(
+		sctx.WithName("test"),
+		sctx.WithComponent(NewGormDB("gorm", "")),
+	)
+
+	if err := testServiceCtx.Load(); err != nil {
+		panic(err)
+	}
+
+	defer testServiceCtx.Stop()
+
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestGormDB_Connect_Success(t *testing.T) {
+	gormComp := testServiceCtx.MustGet("gorm").(*gormDB)
+
+	db := gormComp.db
+	if db == nil {
+		t.Fatal("gorm db should not be nil")
+	}
+}
+
+type TestUser struct {
+	ID   uint
+	Name string
+}
+
+func TestGormDB_InsertFind_Success(t *testing.T) {
+	gormComp := testServiceCtx.MustGet("gorm").(*gormDB)
+	db := gormComp.GetDB()
+
+	err := db.AutoMigrate(&TestUser{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	user := TestUser{Name: "dat"}
+
+	if err := db.Create(&user).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	var found TestUser
+	if err := db.First(&found, "name = ?", "dat").Error; err != nil {
+		t.Fatal(err)
+	}
+}
