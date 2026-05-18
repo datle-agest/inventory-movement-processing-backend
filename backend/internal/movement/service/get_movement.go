d183314390c6fcc32edc64df57763f3b7d99c1e3
BaoLe1703 <IntAGT26.02.012@agest.vn>
2026-05-18 10:17:55 +0700

    feat: add pagination for get movement api

--- backend/internal/movement/service/get_movement.go
+++ backend/internal/movement/service/get_movement.go
@@ -4,13 +4,16 @@ import (
 	"context"
 	"errors"
 	movementEntity "inventory-movement-processing/internal/movement/entity"
+	"inventory-movement-processing/pkg/core"
 )
 
-func (s *service) GetMovementsByItemID(ctx context.Context, itemId int) ([]*movementEntity.Movement, error) {
+func (s *service) GetMovementsByItemID(ctx context.Context, itemId int, paging *core.Pagination) ([]*movementEntity.Movement, error) {
 	if itemId <= 0 {
 		return nil, errors.New("invalid item id for history lookup")
 	}
-	movements, err := s.movementRepo.GetMovementsByItemID(ctx, itemId)
+
+	// Chuyển tiếp con trỏ paging xuống repo gorm xử lý
+	movements, err := s.movementRepo.GetMovementsByItemID(ctx, itemId, paging)
 	if err != nil {
 		return nil, err
 	}
